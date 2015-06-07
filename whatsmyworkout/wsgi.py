e21a19bfcb9806cff80ff3acdeabbdfb060f4187
Ashley Felton <ashley@ropable.com>
2015-06-07 22:02:23 +0800

    Skeleton Django project.

--- whatsmyworkout/wsgi.py
+++ whatsmyworkout/wsgi.py
@@ -1,16 +1,10 @@
 """
 WSGI config for whatsmyworkout project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whatsmyworkout.settings")
-
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whatsmyworkout.settings.base")
+application = Cling(MediaCling(get_wsgi_application()))
