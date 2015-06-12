1088edc09ef02650750f84e6ebf5f05f892f561a
Ashley Felton <ashley@ropable.com>
2015-06-12 13:48:16 +0800

    Update initial migration for workout app.

--- whatsmyworkout/workout/migrations/0001_initial.py
+++ whatsmyworkout/workout/migrations/0001_initial.py
@@ -3,12 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.utils.timezone
+from django.conf import settings
 import workout.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('exercise', '0001_initial'),
         ('auth', '0006_require_contenttypes_0002'),
     ]
 
@@ -26,6 +28,7 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into the admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('workout_target', models.PositiveIntegerField(default=0)),
                 ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
@@ -36,5 +39,30 @@ class Migration(migrations.Migration):
             managers=[
                 ('objects', workout.models.UserManager()),
             ],
+        ),
+        migrations.CreateModel(
+            name='Activity',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('reps', models.PositiveIntegerField()),
+                ('exercise', models.ForeignKey(to='exercise.Exercise')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Workout',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('sets', models.PositiveIntegerField()),
+                ('target_difficulty', models.PositiveIntegerField()),
+                ('generated', models.DateTimeField()),
+                ('delivered', models.DateTimeField(null=True)),
+                ('feedback', models.PositiveSmallIntegerField(null=True, choices=[(0, 'Easy'), (1, 'Okay'), (2, 'Hard')])),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='activity',
+            name='workout',
+            field=models.ForeignKey(to='workout.Workout'),
         ),
     ]
