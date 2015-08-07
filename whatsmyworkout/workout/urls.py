7ee056088bf5e151991a36bc4b5ced29f1de543f
Ashley Felton <ashley@ropable.com>
2015-08-07 13:22:00 +0800

    Stubbed out workout view, template.

--- whatsmyworkout/workout/urls.py
+++ whatsmyworkout/workout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.LandingPage.as_view(), name='landing_page'),
+    url(r'^workout$', views.WorkoutPage.as_view(), name='workout_page'),
 )
--- whatsmyworkout/workout/views.py
+++ whatsmyworkout/workout/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 
 class LandingPage(TemplateView):
+    """Project landing/signup page.
+    """
     template_name = 'workout/landing_page.html'
+
+
+class WorkoutPage(TemplateView):
+    """Page to return a random workout, and accept requests for a harder/easier one.
+    May also serve as an API endpoint.
+    """
+    template_name = 'workout/workout.html'
