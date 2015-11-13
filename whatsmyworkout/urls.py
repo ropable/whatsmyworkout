c926b673e9ae0819e67105779efbbf8551e57799
Ashley Felton <ashley@ropable.com>
2015-11-13 12:36:04 +0800

    Comment out accounts URL path.

--- whatsmyworkout/urls.py
+++ whatsmyworkout/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # Next line is to remove the logout confirmation step.
     #url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     #url(r'^accounts/', include('allauth.urls')),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+    #url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('workout.urls')),
 ]
