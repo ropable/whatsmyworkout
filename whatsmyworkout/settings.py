be43ebc90459f5ad1f1bdb13e7ea274e4ef26977
Ashley Felton <ashley@ropable.com>
2016-08-16 13:27:55 +0800

    Simplify & update base project template.

--- whatsmyworkout/settings.py
+++ whatsmyworkout/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.request',
                 # `allauth` specific context processors
                 #'allauth.account.context_processors.account',
                 #'allauth.socialaccount.context_processors.socialaccount',
--- whatsmyworkout/urls.py
+++ whatsmyworkout/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Next line is to remove the logout confirmation step.
@@ -7,5 +8,6 @@ urlpatterns = [
     #url(r'^accounts/', include('allauth.urls')),
     #url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('workout.urls')),
+    #url(r'^', include('workout.urls')),
+    url(r'^$', TemplateView.as_view(template_name='landing.html'), name='landing'),
 ]
